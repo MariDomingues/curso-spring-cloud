5bc3ba8cc051016e1269b318a937875406047719
Mariane <ma_ap_domingues@hotmail.com>
2021-12-28 15:13:06 -0300

    adjustment in repository

--- loja/src/main/java/com/curso/microservico/loja/repository/CompraRepository.java
+++ loja/src/main/java/com/curso/microservico/loja/repository/CompraRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CompraRepository extends JpaRepository<CompraEntity, Long> {
+public interface CompraRepository extends JpaRepository<Long, CompraEntity> {
 }
