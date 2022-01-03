1a4788d9d53b02d27ed9a5c29afba57c59da430a
Mariane <ma_ap_domingues@hotmail.com>
2022-01-03 12:35:35 -0300

    testing sonar

--- loja/src/main/java/com/curso/microservico/loja/service/ApiService.java
+++ loja/src/main/java/com/curso/microservico/loja/service/ApiService.java
@@ -3,9 +3,11 @@ package com.curso.microservico.loja.service;
 import com.curso.microservico.loja.client.FornecedorClient;
 import com.curso.microservico.loja.model.dto.fornecedor.FornecedorDto;
 import com.curso.microservico.loja.model.dto.fornecedor.PedidoDto;
+import com.curso.microservico.loja.model.dto.fornecedor.TokenDto;
 import com.curso.microservico.loja.model.form.CompraItemDto;
 import com.curso.microservico.loja.model.form.LoginForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -26,13 +28,25 @@ public class ApiService {
         }
     }
 
-    public PedidoDto setPedido(LoginForm pLogin, List<CompraItemDto> pListItem) {
+    public PedidoDto setPedido(LoginForm pLogin, List<CompraItemDto> pListItem) throws Exception {
 
-        return fornecedorClient.realizaPedido(pListItem, getHeader(pLogin)).getBody();
+        ResponseEntity<PedidoDto> pedido = fornecedorClient.realizaPedido(pListItem, getHeader(pLogin));
+
+        if (pedido != null) {
+            return pedido.getBody();
+        }
+
+        throw new Exception("Pedido não Gerado!");
     }
 
-    private String getHeader(LoginForm pLogin) {
+    private String getHeader(LoginForm pLogin) throws Exception {
 
-        return "Bearer " + fornecedorClient.autenticar(pLogin).getBody().getToken();
+        ResponseEntity<TokenDto> autenticar = fornecedorClient.autenticar(pLogin);
+
+        if (autenticar != null) {
+            return "Bearer " + autenticar.getBody().getToken();
+        }
+
+        throw new Exception("Token não Gerado!");
     }
 }
